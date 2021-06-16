09e76da1e08466092e656b0e30b5ef6cf823a22f
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-06-16 17:20:52 +0700

    Generate test failures

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,12 +3,15 @@ import { PlaywrightTestConfig } from "@playwright/test";
 const playwrightConfig: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   timeout: 5 * 60 * 1000,
-  retries: 3,
+  retries: 2,
   use: {
     // Emulate browsing in San Francisco, CA, USA
     locale: "en-US",
     timezoneId: "America/Los_Angeles",
     geolocation: { latitude: 37.773972, longitude: -122.431297 },
+    // Report failure(s)
+    screenshot: "only-on-failure",
+    video: "retry-with-video",
   },
 };
 
