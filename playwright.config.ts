f0689192e51dc356fbc3bafed4ff2d313f525f6a
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-06-16 13:23:03 +0700

    Improve config and scripts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,6 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const playwrightConfig: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
-  retries: 3,
 };
 
 export default playwrightConfig;
