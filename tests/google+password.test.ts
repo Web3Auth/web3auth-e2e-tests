a1579fa1d325a81be54c3bc5c5602ea6269253ef
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-06-16 18:02:57 +0700

    Fix tests on Safari

--- tests/google+password.test.ts
+++ tests/google+password.test.ts
@@ -5,13 +5,18 @@ test.use({
   storageState: "state/0-signed-in-google.json",
 });
 
-test.only("Login with Google + Password", async ({ page }) => {
+test("Login with Google + Password", async ({ page, browserName }) => {
   // Login with Google
   await page.goto("https://app.openlogin.com");
   await page.click('button:has-text("Get Started")');
   await page.click('button:has-text("Continue with Google")');
   await page.waitForURL("https://accounts.google.com/**");
   await page.click(`text=${creds.google.email}`);
+
+  if (browserName === "webkit")
+    // Workaround wait for URL issue on Safari
+    while (!page.url().startsWith("https://app.openlogin.com"))
+      await page.waitForTimeout(100);
 
   // Enter password
   await page.waitForURL("https://app.openlogin.com/tkey-input#**");
