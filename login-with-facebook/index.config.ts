a2e29a19b4aee5ed32bae05f197e7141720c74dd
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-07-24 12:57:50 +0700

    Add more Facebook login, share config

--- login-with-facebook/index.config.ts
+++ login-with-facebook/index.config.ts
@@ -4,6 +4,7 @@ import {
   PlaywrightWorkerOptions,
 } from "@playwright/test";
 import { TestArgs } from "./index.base";
+import indexConfig from "../index.config";
 
 const projects: Array<
   Pick<PlaywrightWorkerOptions, "browserName"> & Omit<TestArgs, "openloginURL">
@@ -15,24 +16,18 @@ const projects: Array<
       name: "Elizabeth",
     },
   },
+  {
+    browserName: "firefox",
+    user: {
+      email: "dvpizamosb_1624264537@tfbnw.net",
+      name: "Lisa",
+    },
+  },
 ];
 
 const config: PlaywrightTestConfig<TestArgs> = {
+  ...indexConfig,
   testDir: __dirname,
-  forbidOnly: !!process.env.CI,
-  timeout: process.env.CI ? 5 * 60 * 1000 : 0,
-  retries: process.env.CI ? 3 : 0,
-  use: {
-    // Emulate browsing in San Francisco, CA, USA
-    locale: "en-US",
-    timezoneId: "America/Los_Angeles",
-    geolocation: { latitude: 37.773972, longitude: -122.431297 },
-
-    // Report failure(s)
-    screenshot: "only-on-failure",
-    video: process.env.CI ? "retry-with-video" : "retain-on-failure",
-    trace: process.env.CI ? "off" : "retain-on-failure",
-  },
   projects: projects.map(({ browserName, user }) => ({
     name: browserName,
     use: {
--- login-with-google/index.config.ts
+++ login-with-google/index.config.ts
@@ -4,6 +4,7 @@ import {
   PlaywrightWorkerOptions,
 } from "@playwright/test";
 import { TestArgs } from "./index.base";
+import indexConfig from "../index.config";
 
 const projects: Array<
   Pick<PlaywrightWorkerOptions, "browserName"> & Omit<TestArgs, "openloginURL">
@@ -19,21 +20,8 @@ const projects: Array<
 ];
 
 const config: PlaywrightTestConfig<TestArgs> = {
+  ...indexConfig,
   testDir: __dirname,
-  forbidOnly: !!process.env.CI,
-  timeout: process.env.CI ? 5 * 60 * 1000 : 0,
-  retries: process.env.CI ? 3 : 0,
-  use: {
-    // Emulate browsing in San Francisco, CA, USA
-    locale: "en-US",
-    timezoneId: "America/Los_Angeles",
-    geolocation: { latitude: 37.773972, longitude: -122.431297 },
-
-    // Report failure(s)
-    screenshot: "only-on-failure",
-    video: process.env.CI ? "retry-with-video" : "retain-on-failure",
-    trace: process.env.CI ? "off" : "retain-on-failure",
-  },
   projects: projects.map(({ browserName, user }) => ({
     name: browserName,
     use: {
