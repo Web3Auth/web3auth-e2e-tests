e754346d6c0a42df7daf866fd6e7370a1623d872
Naveed Elahi <statix231@hotmail.com>
2021-12-17 20:06:42 +0800

    duplicate new v2 user workflow and test files

---
+++ openlogin/existing-v2-user/index.config.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import {
+  PlaywrightTestConfig,
+  PlaywrightWorkerOptions,
+} from "@playwright/test";
+import { TestArgs } from "./index.lib";
+import indexConfig from "../../index.config";
+
+const projects: Array<
+Pick<PlaywrightWorkerOptions, "browserName"> & Omit<TestArgs, "openloginURL">
+> = [
+  {
+    browserName: "chromium",
+    user: { email: `testuser${process.env.TEST_RUN_ID}@openlogin.com` }
+  },
+];
+
+const config: PlaywrightTestConfig = {
+  ...indexConfig,
+  testDir: __dirname,
+  projects: projects.map(({ browserName, user }) => ({
+    name: browserName,
+    use: {
+      browserName,
+      storageState: path.resolve(__dirname, `${browserName}.json`),
+      user
+    },
+  })),
+};
+
+export default config;
---
+++ openlogin/existing-v2-user/index.test.ts
@@ -0,0 +1,42 @@
+const { expect } = require('@playwright/test');
+import { confirmEmail } from "../../utils";
+import { test } from "./index.lib";
+
+test('new v2 user can log in correctly', async ({
+  context,
+  page,
+  openloginURL,
+  user
+}) => {
+  await page.goto(openloginURL);
+  await page.click('button:has-text("Get Started")');
+
+  // Login with Passwordless
+  const timestamp = Math.floor(Date.now() / 1000);
+  await page.fill('[placeholder="Email"]', user.email);
+  await page.click('button:has-text("Continue with Email")');
+  await page.waitForSelector("text=email has been sent");
+  expect(await page.isVisible(`text=${user.email}`)).toBeTruthy();
+
+  // Confirm email
+  test.fixme(
+    !(await confirmEmail({
+      context,
+      timestamp,
+      resend: () => page.click("text=Resend"),
+    }))
+  );
+
+  // Should be signed in in <2 minutes
+  await page.waitForURL(`${openloginURL}/wallet/home`, {
+    timeout: 2 * 60 * 1000,
+  });
+
+  // Go to Account page
+  await Promise.all([page.waitForNavigation(), page.click("text=Account")]);
+  expect(await page.isVisible(`text=${user.email}`)).toBeTruthy();
+
+  // Logout
+  await Promise.all([page.waitForNavigation(), page.click("text=Logout")]);
+  expect(page.url()).toBe(`${openloginURL}/`);
+});
