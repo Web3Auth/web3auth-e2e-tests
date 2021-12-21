1a633a579bbff49f294d07af6d1ad4d6e977ae2e
Naveed Elahi <statix231@hotmail.com>
2021-12-21 13:00:42 +0800

    Test facebook + two-fa(backup phrase) flow (#60)
    * duplicate login fb workflow
    
    * rename fb 2fa workflow file
    
    * create fb 2fa workflow
    
    * create fb 2fa e2e test
    
    * add firefox to list of projects
    
    * refactor backupPhrase usage
    
    * dry out user reference in projects
    
    * update status endpoint
    
    * change test ac
    
    * test only chromium
    
    * remove update status job
    
    * correct the email address
    
    * remove testing firefox from workflow
    
    * rename workflow and job name
    
    * remove unused status service id env
    
    * add backup phrase to git ignore
    
    * enforce max timeout for nav to wallet/home

---
+++ openlogin/login-with-facebook-2fa/index.config.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import {
+  PlaywrightTestConfig,
+  PlaywrightWorkerOptions,
+} from "@playwright/test";
+import { TestArgs } from "./index.lib";
+import indexConfig from "../../index.config";
+import { readFileSync } from "fs";
+
+const user = {
+  email: "chdrotfhac_1624264537@tfbnw.net",
+  name: "Jennifer",
+  backupPhrase: readFileSync(path.resolve(__dirname, "backup-phrase.txt")).toString()
+}
+
+const projects: Array<
+  Pick<PlaywrightWorkerOptions, "browserName"> & Omit<TestArgs, "openloginURL">
+> = [
+  {
+    browserName: "chromium",
+    user
+  }
+];
+
+const config: PlaywrightTestConfig<TestArgs> = {
+  ...indexConfig,
+  testDir: __dirname,
+  projects: projects.map(({ browserName, user }) => ({
+    name: browserName,
+    use: {
+      browserName,
+      storageState: path.resolve(__dirname, `${browserName}.json`),
+      user,
+    },
+  })),
+};
+
+export default config;
