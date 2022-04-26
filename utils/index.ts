57f25471e5c6030ee62b2bd600a3363063713ba0
Shubham Pandey <shubhampandey@Shubhams-MacBook-Air.local>
2022-04-26 15:07:28 +0530

    introduced timeout for a click event

--- utils/index.ts
+++ utils/index.ts
@@ -158,7 +158,9 @@ async function setup2FA(page: Page, flow: string) {
     await page.click(".v-input--selection-controls__ripple");
     await page.click('button:has-text("Save current device")');
   }
-  await page.click('button:has-text("View advanced option")');
+  await page.click('button:has-text("View advanced option")', {
+    timeout: 10 * 1000,
+  });
   const [download] = await Promise.all([
     page.waitForEvent("download"),
     page.click('button:has-text("Download my recovery phrase")'),
