41ae5239c475ebf165da175d7bb19349a6e60b51
Sidharth Suresh <sidharth@Sidharths-MacBook-Pro-2.local>
2023-01-11 21:23:36 +0530

    accnt 2 tests

--- openlogin/account-page/index.config.ts
+++ openlogin/account-page/index.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import {
   PlaywrightTestConfig,
   PlaywrightWorkerOptions,
@@ -8,17 +9,39 @@ import indexConfig from "../../index.config";
 const projects: Array<
   Pick<PlaywrightWorkerOptions, "browserName"> & Omit<TestArgs, "openloginURL">
 > = [
-  { browserName: "chromium" },
-  { browserName: "firefox" },
-  { browserName: "webkit" },
+  {
+    browserName: "chromium",
+    user: {
+      email: "woiovgalay_1624264537@tfbnw.net",
+      name: "Elizabeth",
+    },
+  },
+  {
+    browserName: "firefox",
+    user: {
+      email: "dvpizamosb_1624264537@tfbnw.net",
+      name: "Lisa",
+    },
+  },
+  {
+    browserName: "webkit",
+    user: {
+      email: "dvpizamosb_1624264537@tfbnw.net",
+      name: "Lisa",
+    },
+  },
 ];
 
 const config: PlaywrightTestConfig<TestArgs> = {
   ...indexConfig,
   testDir: __dirname,
-  projects: projects.map(({ browserName }) => ({
+  projects: projects.map(({ browserName, user }) => ({
     name: browserName,
-    use: { browserName },
+    use: {
+      browserName,
+      storageState: path.resolve(__dirname, `${browserName}.json`),
+      user,
+    },
   })),
 };
 
--- openlogin/account-page/index.test.ts
+++ openlogin/account-page/index.test.ts
@@ -1,23 +1,120 @@
 import { expect } from "@playwright/test";
 import { test } from "./index.lib";
+import { signInWithFacebook, useAutoCancel2FASetup } from "../../utils";
+import { useAutoCancelShareTransfer } from "../../utils/index";
 
-test.describe("On homepage,", () => {
+test.describe("On accounts page,", () => {
   test.beforeEach(async ({ page, openloginURL }) => {
     await page.goto(openloginURL);
   });
 
-  test.only(`title should be "OpenLogin"`, async ({ page }) => {
-    const title = await page.title();
-    expect(title).toBe("OpenLogin");
-  });
-  test.only(`there should be "Get Started" button`, async ({ page }) => {
-    expect(await page.isVisible(`button:has-text("Get Started")`)).toBeTruthy();
+  test(`page title should be "Account" for account page`, async ({
+    page,
+    openloginURL,
+    FB,
+  }) => {
+    page.setDefaultTimeout(8 * 60 * 1000);
+    page.setDefaultNavigationTimeout(8 * 60 * 1000);
+
+    // Login with Facebook
+    await page.goto(openloginURL);
+    await page.click('button:has-text("Get Started")');
+    await page.click('[aria-label="login with facebook"]');
+    await signInWithFacebook({ page, FB });
+
+    await useAutoCancelShareTransfer(page);
+    await useAutoCancel2FASetup(page);
+
+    await page.waitForURL(`${openloginURL}/wallet/home`, {
+      waitUntil: "load",
+    });
+
+    expect(page.url()).toBe(`${openloginURL}/wallet/home`);
+
+    await page.goto(`${openloginURL}/wallet/account`);
+    await page.waitForURL(`${openloginURL}/wallet/account`, {
+      waitUntil: "load",
+    });
+    expect(page.url()).toBe(`${openloginURL}/wallet/account`);
+    expect(await page.isVisible("text=Account")).toBeTruthy();
+
+    await Promise.all([page.waitForNavigation(), page.click("text=Logout")]);
+    expect(page.url()).toBe(`${openloginURL}/`);
   });
 
-  test.only(`click on "Get Started" should show login modal`, async ({
+  test(`should display 2FA enable window for single factor account `, async ({
     page,
+    openloginURL,
+    FB,
   }) => {
-    await page.click(`button:has-text("Get Started")`);
-    expect(await page.isVisible("text=Welcome onboard")).toBeTruthy();
+    page.setDefaultTimeout(8 * 60 * 1000);
+    page.setDefaultNavigationTimeout(8 * 60 * 1000);
+
+    // Login with Facebook
+    await page.goto(openloginURL);
+    await page.click('button:has-text("Get Started")');
+    await page.click('[aria-label="login with facebook"]');
+    await signInWithFacebook({ page, FB });
+
+    await useAutoCancelShareTransfer(page);
+    await useAutoCancel2FASetup(page);
+
+    await page.waitForURL(`${openloginURL}/wallet/home`, {
+      waitUntil: "load",
+    });
+
+    expect(page.url()).toBe(`${openloginURL}/wallet/home`);
+
+    await page.goto(`${openloginURL}/wallet/account`);
+    await page.waitForURL(`${openloginURL}/wallet/account`, {
+      waitUntil: "load",
+    });
+    expect(page.url()).toBe(`${openloginURL}/wallet/account`);
+    expect(
+      await page.isVisible(
+        "text=We strongly recommend you to enable 2FA on your account"
+      )
+    ).toBeTruthy();
+
+    await Promise.all([page.waitForNavigation(), page.click("text=Logout")]);
+    expect(page.url()).toBe(`${openloginURL}/`);
   });
+
+  //   test(`should change account password successfully`, async ({
+  //     page,
+  //     openloginURL,
+  //     FB,
+  //   }) => {
+  //     page.setDefaultTimeout(8 * 60 * 1000);
+  //     page.setDefaultNavigationTimeout(8 * 60 * 1000);
+
+  //     // Login with a google account
+  //     await page.goto(openloginURL);
+  //     await page.click('button:has-text("Get Started")');
+  //     await page.click('[aria-label="Continue with Google"]');
+  //     await signInWithFacebook({ page, FB });
+
+  //     await useAutoCancelShareTransfer(page);
+  //     await useAutoCancel2FASetup(page);
+
+  //     await page.waitForURL(`${openloginURL}/wallet/home`, {
+  //       waitUntil: "load",
+  //     });
+
+  //     expect(page.url()).toBe(`${openloginURL}/wallet/home`);
+
+  //     await page.goto(`${openloginURL}/wallet/account`);
+  //     await page.waitForURL(`${openloginURL}/wallet/account`, {
+  //       waitUntil: "load",
+  //     });
+  //     expect(page.url()).toBe(`${openloginURL}/wallet/account`);
+  //     expect(
+  //       await page.isVisible(
+  //         "text=We strongly recommend you to enable 2FA on your account"
+  //       )
+  //     ).toBeTruthy();
+
+  //     await Promise.all([page.waitForNavigation(), page.click("text=Logout")]);
+  //     expect(page.url()).toBe(`${openloginURL}/`);
+  //   });
 });
