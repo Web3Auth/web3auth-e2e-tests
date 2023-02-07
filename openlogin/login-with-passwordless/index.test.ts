1ab0b0fb821f913c9639be4dd70e2c4828b1ee4b
Sidharth Suresh <sidharth@Sidharths-MacBook-Pro-2.local>
2023-02-07 23:48:07 +0530

    new pipe

--- openlogin/login-with-passwordless/index.test.ts
+++ openlogin/login-with-passwordless/index.test.ts
@@ -1,10 +1,48 @@
-import { expect } from "@playwright/test";
+// import { expect } from "@playwright/test";
+// import {
+//   useAutoCancel2FASetup,
+//   signInWithEmail,
+//   generateRandomEmail,
+// } from "../../utils";
+// import { test } from "./index.lib";
+// import { useAutoCancelShareTransfer } from "../../utils/index";
+// import Mailosaur from "mailosaur";
+
+// const mailosaur = new Mailosaur(process.env.MAILOSAUR_API_KEY || "");
+
+// const testEmail = generateRandomEmail();
+
+// test("Login with passwordless", async ({ browser, page, openloginURL }) => {
+//   page.setDefaultTimeout(8 * 60 * 1000);
+//   page.setDefaultNavigationTimeout(8 * 60 * 1000);
+
+//   // Verify environment variables
+//   expect(
+//     !!process.env.MAILOSAUR_SERVER_ID &&
+//       !!process.env.MAILOSAUR_API_KEY &&
+//       !!process.env.MAILOSAUR_SERVER_DOMAIN
+//   ).toBe(true);
+
+//   await page.goto(openloginURL);
+//   await signInWithEmail(page, testEmail, browser);
+
+//   // Successful login
+//   await useAutoCancelShareTransfer(page);
+//   await useAutoCancel2FASetup(page);
+
+//   await page.waitForURL(`${openloginURL}/wallet/home`);
+//   expect(page.url()).toBe(`${openloginURL}/wallet/home`);
+//   const welcome = await page.waitForSelector(`text=Welcome`);
+// });
+
+import { expect, Page } from "@playwright/test";
+import { test } from "./index.lib";
 import {
   useAutoCancel2FASetup,
   signInWithEmail,
   generateRandomEmail,
+  catchError,
 } from "../../utils";
-import { test } from "./index.lib";
 import { useAutoCancelShareTransfer } from "../../utils/index";
 import Mailosaur from "mailosaur";
 
@@ -12,25 +50,31 @@ const mailosaur = new Mailosaur(process.env.MAILOSAUR_API_KEY || "");
 
 const testEmail = generateRandomEmail();
 
-test("Login with passwordless", async ({ browser, page, openloginURL }) => {
-  page.setDefaultTimeout(8 * 60 * 1000);
-  page.setDefaultNavigationTimeout(8 * 60 * 1000);
+test.describe.serial("Passwordless Login", () => {
+  let page: Page;
+  test.beforeAll(async ({ openloginURL, browser }) => {
+    test.setTimeout(60000); // adding more time to compensate high loading time
+    const context = await browser.newContext({});
+    page = await context.newPage();
+    await page.goto(openloginURL);
+    await signInWithEmail(page, testEmail, browser);
+    await catchError(page);
+    await useAutoCancelShareTransfer(page);
+    await useAutoCancel2FASetup(page);
+    await page.waitForURL(`${openloginURL}/wallet/home`, {
+      waitUntil: "load",
+    });
+  });
+  test.afterAll(async ({ browser }) => {
+    browser.close();
+  });
 
-  // Verify environment variables
-  expect(
-    !!process.env.MAILOSAUR_SERVER_ID &&
-      !!process.env.MAILOSAUR_API_KEY &&
-      !!process.env.MAILOSAUR_SERVER_DOMAIN
-  ).toBe(true);
-
-  await page.goto(openloginURL);
-  await signInWithEmail(page, testEmail, browser);
-
-  // Successful login
-  await useAutoCancelShareTransfer(page);
-  await useAutoCancel2FASetup(page);
-
-  await page.waitForURL(`${openloginURL}/wallet/home`);
-  expect(page.url()).toBe(`${openloginURL}/wallet/home`);
-  const welcome = await page.waitForSelector(`text=Welcome`);
+  test(`should login with passwordless and reach home page`, async ({
+    openloginURL,
+  }) => {
+    await page.waitForURL(`${openloginURL}/wallet/home`, {
+      waitUntil: "load",
+    });
+    expect(await page.isVisible(`text=${testEmail}`)).toBeTruthy();
+  });
 });
