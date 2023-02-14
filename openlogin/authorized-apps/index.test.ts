09e8e71420b3411ec4cd6bda3cea64a3d6479d1b
shubham <16849485+metallicalfa2@users.noreply.github.com>
2023-02-14 11:21:55 +0800

    feat: updates

--- openlogin/authorized-apps/index.test.ts
+++ openlogin/authorized-apps/index.test.ts
@@ -15,9 +15,9 @@ const mailosaur = new Mailosaur(process.env.MAILOSAUR_API_KEY || "");
 const testEmail = generateRandomEmail();
 
 test.describe.serial("App authorization page test", () => {
-  test.skip(() => process.env.PLATFORM === "local"); // skipping this test for local
-  test.skip(() => process.env.PLATFORM === "testing"); // skipping this test for testing env
-  test.skip(() => process.env.PLATFORM === "cyan"); // skipping this test for cyan since 100thieves is too slow.
+  // test(() => process.env.PLATFORM === "local"); //ping this test for local
+  // test(() => process.env.PLATFORM === "testing"); //ping this test for testing env
+  // test(() => process.env.PLATFORM === "cyan"); //ping this test for cyan since 100thieves is too slow.
   let page: Page;
   test.beforeAll(async ({ browser, openloginURL }) => {
     test.setTimeout(60000); // adding more time to compensate high loading time
@@ -36,7 +36,7 @@ test.describe.serial("App authorization page test", () => {
   test.afterAll(async ({ browser }) => {
     browser.close();
   });
-  test.skip(`should display "You are not connected to any applications yet." when no apps are connected.`, async ({
+  test(`should display "You are not connected to any applications yet." when no apps are connected.`, async ({
     openloginURL,
   }) => {
     await page.goto(`${openloginURL}/wallet/apps`);
@@ -53,7 +53,7 @@ test.describe.serial("App authorization page test", () => {
 
   // Below test use testEmail to login to solana wallet prod and there by generates an app share
   // which gets listed in list of apps
-  test.skip(`should connect DApp  wallet with passwordless login and list app`, async ({
+  test(`should connect DApp  wallet with passwordless login and list app`, async ({
     openloginURL,
     browser,
   }) => {
@@ -88,6 +88,51 @@ test.describe.serial("App authorization page test", () => {
       );
       await page3.close();
       await page2.waitForURL(`https://solana.tor.us/wallet/home`, {
+        waitUntil: "load",
+      });
+      await page.reload();
+      await page.waitForURL(`${openloginURL}/wallet/apps`, {
+        waitUntil: "load",
+      });
+
+      expect(page.url()).toBe(`${openloginURL}/wallet/apps`);
+      await page.waitForSelector("text=solana-prod");
+      expect(await page.isVisible("text=Authorized Apps")).toBeTruthy();
+      expect(
+        await page.isVisible(
+          "text=You are not connected to any applications yet."
+        )
+      ).toBeFalsy();
+      expect(await page.isVisible("text=solana-prod")).toBeTruthy();
+      await page2.close();
+    }
+    if (process.env.PLATFORM === "beta") {
+      await page2.goto("https://solana-testing.tor.us/login");
+      await page2.fill('[placeholder="Enter your email"]', testEmail);
+      await page2.click('button:has-text("Continue with Email")');
+      const newEmail = await mailosaur.messages.get(
+        process.env.MAILOSAUR_SERVER_ID || "",
+        {
+          sentTo: testEmail,
+        }
+      );
+      expect(newEmail.subject).toBe("Verify your email");
+      let link = findLink(newEmail.html?.links || [], "Confirm my email");
+      if (!link) {
+        link = findLink(newEmail.html?.links || [], "Verify my email");
+      }
+      expect(link?.text).toBe("Verify my email");
+      const href = link?.href || "";
+      const page3 = await context3.newPage();
+      await page3.goto(href);
+      await page3.waitForSelector(
+        "text=Close this and return to your previous window",
+        {
+          timeout: 10000,
+        }
+      );
+      await page3.close();
+      await page2.waitForURL(`https://solana-testing.tor.us/wallet/home`, {
         waitUntil: "load",
       });
       await page.reload();
@@ -151,9 +196,12 @@ test.describe.serial("App authorization page test", () => {
     }
   });
 
-  test.skip(`should be able to delete app share from UI`, async ({
+  test(`should be able to delete app share from UI`, async ({
     openloginURL,
   }) => {
+    if (!["prod", "cyan"].includes(process.env.PLATFORM || "")) {
+      return
+    }
     await page.click('button[aria-label="delete device share"]');
     await page.goto(`${openloginURL}/wallet/apps`);
     await page.waitForURL(`${openloginURL}/wallet/apps`, {
