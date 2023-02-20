da0ec61463612d5924d1f1343e26f54959a5e20d
Pooja Rani <chopraapooja@gmail.com>
2023-02-20 11:37:00 +0530

    feat: updates

--- bin/runTestsCI.sh
+++ bin/runTestsCI.sh
@@ -7,7 +7,7 @@ for dir in ${dirs[@]}; do
   for child_dir in "$dir"/*; do
     if [[ -d "$child_dir" ]]; then
       echo "$child_dir"
-      npm run test $child_dir
+      npm run test:trace $child_dir
       code=$?
       echo $code
       if [[ "$code" -ne 0 ]]; then
@@ -21,6 +21,13 @@ if [[ $exitcode -ne 0 ]] then
     curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "${{$CHAT_ID}}", "text": "TestResult=Tests failed"}' https://api.telegram.org/bot$BOT_API_TOKEN/sendMessage
 else
     echo "Test Passed, no alert!"
+fi
+
+TEST_RESULT_DIR="./test-results"
+if [ -d "$TEST_RESULT_DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "Listing content of ${TEST_RESULT_DIR}..."
+  ls $TEST_RESULT_DIR
 fi
 
 exit $exitcode
