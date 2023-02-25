ccbb6dc6bc65638dcfe7f66be58b577395d329da
Swaraj Phadtare <1swarajphadtare@gmail.com>
2023-02-25 12:46:48 +0530

    path for tests

--- bin/runTestsCI.sh
+++ bin/runTestsCI.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-dirs=("./openlogin" "./wallet")
+dirs=("./openlogin")
 
 exitcode=0
+TEST_RESULT_DIR="./test-results"
+
 for dir in ${dirs[@]}; do
   for child_dir in "$dir"/*; do
     if [[ -d "$child_dir" ]]; then
       echo "$child_dir"
-      npm run test:trace $child_dir
+      npm run test:trace "test-results-${child_dir:2}" $child_dir
       code=$?
       echo $code
       if [[ "$code" -ne 0 ]]; then
@@ -17,14 +19,13 @@ for dir in ${dirs[@]}; do
   done
 done
 if [[ $exitcode -ne 0 ]]; then
-    echo "Test Failed, sending telegram alert"
+  echo "Test Failed, sending telegram alert"
   echo "*******sending telegram alert*********"
-    curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "${{$CHAT_ID}}", "text": "TestResult=Tests failed"}' https://api.telegram.org/bot$BOT_API_TOKEN/sendMessage
+  curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "${{$CHAT_ID}}", "text": "TestResult=Tests failed"}' https://api.telegram.org/bot$BOT_API_TOKEN/sendMessage
 else
     echo "Test Passed, no alert!"
 fi
 
-TEST_RESULT_DIR="./test-results"
 if [ -d "$TEST_RESULT_DIR" ]; then
   # Take action if $DIR exists. #
   echo "*******listing content of test-results*********"
