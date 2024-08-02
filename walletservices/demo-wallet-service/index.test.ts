5ee0e4d8a6cd51ba67127936308936fe3a2c8e5a
vinayaktorus <vinayak@tor.us>
2024-08-02 10:09:10 +0530

    balance updated

--- walletservices/demo-wallet-service/index.test.ts
+++ walletservices/demo-wallet-service/index.test.ts
@@ -6,15 +6,16 @@ import { catchErrorAndExit, signInWithEmailWithTestEmailApp, signInWithEmailWith
 import { DemoWalletServicesPage } from "./DemoWalletServicesPage";
 let sessionId: string = "";
 const demoWalletServiceLoginURL = "https://demo-wallet.web3auth.io";
+const authServiceLoginURL = "https://demo-openlogin-v8.web3auth.io/";
 const testEmail = "kelg8.j5s90ldb0b35@inbox.testmail.app";
 const currentTimestamp = Math.floor(Date.now() / 1000);
 const address = "0x0dB...d4e49F";
 const walletAddress = "0x0dBa...4e49F";
-const balance = "0.635632785708915";
+const expectedBalance = "0.635632785708915";
 
 test.describe.serial("Demo Wallet Services Scenarios @demo", () => {
   test(`Verify demo wallet services functionalities using passwordless login`, async ({ page, browser }) => {
-    test.slow(); // adding more time to compensate high loading time
+    test.setTimeout(3 * 60000); // adding more time to compensate high loading time test.slow() does not help in this case
     const demoWalletServicesPage = new DemoWalletServicesPage(page);
     await page.goto(demoWalletServiceLoginURL);
     await page.waitForLoadState();
@@ -29,7 +30,7 @@ test.describe.serial("Demo Wallet Services Scenarios @demo", () => {
     await demoWalletServicesPage.verifyUserInfoInDemoApp(testEmail);
     await demoWalletServicesPage.switchChain(browser);
     await demoWalletServicesPage.verifyAddressInDemoApp(address);
-    await demoWalletServicesPage.verifyBalanceInDemoApp(balance);
+    await demoWalletServicesPage.verifyBalanceInDemoApp(expectedBalance);
     await demoWalletServicesPage.verifyWalletInDemoApp(walletAddress);
     await demoWalletServicesPage.verifySignedMessages("Personal Sign", browser);
     await demoWalletServicesPage.verifySignedMessages("ETH Sign", browser);
@@ -44,7 +45,7 @@ test.describe.serial("Demo Wallet Services Scenarios @demo", () => {
   test(`Verify user is able to login into wallet services using session id from auth service`, async ({ page, browser }) => {
     test.slow();
     const demoWalletServicesPage = new DemoWalletServicesPage(page);
-    await page.goto("https://demo-openlogin-v8.web3auth.io/");
+    await page.goto(authServiceLoginURL);
     await signInWithEmailWithTestEmailApp(page, testEmail, browser, testEmail.split("@")[0].split(".")[1], currentTimestamp);
     const accountsPage = new AccountsPage(page);
     const keys: string | null = await accountsPage.getOpenLoginState();
