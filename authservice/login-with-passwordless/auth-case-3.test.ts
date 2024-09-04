3f491ffea8c7b8434c49bc6cdbc051253a159a76
Harry Le <levinh.hcmut@gmail.com>
2024-09-04 09:58:14 +0700

    test: update new mfa flow

--- authservice/login-with-passwordless/auth-case-3.test.ts
+++ authservice/login-with-passwordless/auth-case-3.test.ts
@@ -18,7 +18,7 @@ test.describe.serial("Passwordless Login scenarios", () => {
 
     await loginPage.gotoLoginPage(authServiceURL);
     await loginPage.selectBuildEnv("staging");
-    await loginPage.selectAllMFAFactor();
+    await loginPage.selectMFAFactor(["PASSWORD", "AUTHENTICATOR"]);
     await loginPage.selectMFALevel("mandatory");
     await loginPage.selectMFAMandantory(["PASSWORD", "AUTHENTICATOR"]);
     await loginPage.selectOpenloginNetwork("mainnet");
@@ -45,29 +45,17 @@ test.describe.serial("Passwordless Login scenarios", () => {
 
     await authServicePage.clickSetup2FA();
 
-    // SKIP DEVICE FACTOR
-
-    await authServicePage.skipTheFactorSetup();
-
-    // SKIP SOCIAL FACTOR
-
-    await authServicePage.skipTheFactorSetup();
-
     // SETUP AUTHENTICATOR FACTOR
 
-    const secret = await authServicePage.setupAuthenticator();
-
-    // SKIP RECOVERY FACTOR
-
-    await authServicePage.skipTheFactorSetup();
+    const secret = await authServicePage.setupAuthenticatorNewMFAFlow();
 
     // SETUP PASSWORD
 
-    await authServicePage.inputPasswordFactor(passwordTestingFactor);
+    await authServicePage.inputPasswordFactorNewMFAFlow(passwordTestingFactor);
 
     // SKIP PASSKEY
 
-    await authServicePage.skipPasskeySetup();
+    await authServicePage.finishSetupNewMFAList();
     await authServicePage.confirmDone2FASetup();
 
     const privateKey = await dashboardPage.getOpenLoginPrivateKey();
